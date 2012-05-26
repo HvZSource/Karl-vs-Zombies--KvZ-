2ba7700df639a8620dacf49507349899b927dcbe
vantage509 <travis.farrar@gmail.com>
2012-05-25 17:59:35 -0700

    * db.sql  - Added auto-increment primary key 'id' to *_admin table  - Added feed_limit and starve_time to *_timezone table (using as game settings table)  - *_users table changes: Made id the primary key. Allowed null for pic_path and killed_by. Added defaults to state, kills and oz_opt. Removed "on update" from killed to fix bug. Added lifetime_kills, games_completed and active.
    * admin/deactivate_player.php
     - File to deactivate user account before being able to delete the account
    
    NEW IMAGES
     - images/OZ.jpg: profile pic for phantom Original Zombie
     - images/zom_no_photo.jpg: default pic when user has no profile pic

---
+++ admin/deactivate_player.php
@@ -0,0 +1,53 @@
+<?php
+ob_start();
+session_start();
+require_once('security.php');
+require_once('../functions/load_config.php');
+require_once('../functions/quick_con.php');
+$config = load_config('../settings/config.dat');
+$sql = my_quick_con($config) or die("MySQL problem");
+
+$table_u = $config['user_table'];
+$pid = $_GET['id'];
+
+?>
+
+<html>
+<head>
+<link rel='stylesheet' type='text/css' href='style/main.css'>
+</head>
+<?php
+if($_POST['submit'] == 'Deactivate') {
+	$pid = $_POST['id'];
+	$ret = mysql_query("SELECT fname, lname, username FROM $table_u WHERE id='$pid';");
+	while($row = mysql_fetch_array($ret)) {	
+		echo("Sucessfully deactivated player " . $row['fname'] . " " . $row['lname'] . ".");
+	}
+	mysql_query("UPDATE $table_u set active = 0 WHERE id = '$pid';");
+}
+else {
+       $ret = mysql_query("SELECT fname, lname, username FROM $table_u WHERE id='$pid';");
+while($row = mysql_fetch_array($ret))
+{
+	echo("Are you sure you want to deactivate player " . $row['fname'] . " " . $row['lname'] . "?<br />");
+}
+?>
+<body>
+<br />
+<form method=POST action="deactivate_player.php">
+<input type="hidden" name="id" value="<?= $pid; ?>" />
+<input type='submit' name='submit' value='Deactivate'></td></tr>
+</form>
+
+</body>
+
+<?php
+mysql_free_result($ret);
+}
+?>
+</html>
+
+<?php
+mysql_close($sql);
+ob_end_flush();
+?>
